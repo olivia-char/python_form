490ce90da58a563e089278db3f804f75f97c5824
Olivia Stalcup <ocstalcup@gmail.com>
2017-02-08 14:01:22 -0800

    first commit for form

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+	return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+
+	name = request.form['name']
+	location = request.form['location']
+	language = request.form['language']
+	comments = request.form['comments'] 
+
+	return render_template('result.html', name=name, location=location,language=language, comments=comments)
+app.run(debug=True)
